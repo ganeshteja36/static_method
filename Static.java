01daae4be21ba6ef2e60bbaa8c02b641c554f8fe
ganeshteja36 <57834999+ganeshteja36@users.noreply.github.com>
2020-03-16 15:38:25 +0530

    Create Static.java

---
+++ Static.java
@@ -0,0 +1,40 @@
+
+ 
+class Student{  
+     int rollno;  
+     String name;  
+     static String college = "ITS";  
+      
+     static void change(){  
+     college = "VVIT";  
+     }  
+     
+     Student(int r, String n){  
+     rollno = r;  
+     name = n;  
+     }  
+       
+     void display(){System.out.println(rollno+" "+name+" "+college);}  
+}  
+
+public class TestStaticMethod{  
+    public static void main(String args[]){  
+    Student.change();
+    
+    Student s1 = new Student(111,"Karan");  
+    Student s2 = new Student(222,"Aryan");  
+    Student s3 = new Student(333,"Sonoo");  
+   
+    s1.display();  
+    s2.display();  
+    s3.display();  
+    }  
+ 
+class Counter{  
+static int count=0;
+  
+Counter(){  
+count++;
+System.out.println(count);  
+}  
+}
